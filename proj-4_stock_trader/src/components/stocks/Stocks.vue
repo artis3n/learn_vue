c6fa6e4ecd5925f0c876dad3b0003a82f0298565
Artis3n <dev@artis3nal.com>
2020-01-04 12:33:20 -0500

    Set up Vuex and a bunch of other logic

--- proj-4_stock_trader/src/components/stocks/Stocks.vue
+++ proj-4_stock_trader/src/components/stocks/Stocks.vue
@@ -1,10 +1,21 @@
 <template>
-  <h1>The Stocks Component</h1>
+  <div>
+    <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+  </div>
 </template>
 
 <script>
+  import Stock from "./Stock.vue";
     export default {
-        name: "Stocks"
+        name: "Stocks",
+        components: {
+            appStock: Stock,
+        },
+        computed: {
+            stocks() {
+                return this.$store.getters.stocks;
+            }
+        }
     }
 </script>
 
